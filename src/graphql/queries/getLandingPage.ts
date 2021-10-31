2ce28c4e740cb4a790545b35dd87ab51579c97eb
gabxyz <gabxyzdev@gmail.com>
2021-10-31 13:34:47 -0300

    add queries to get data from api

--- src/graphql/queries/getLandingPage.ts
+++ src/graphql/queries/getLandingPage.ts
@@ -1,10 +1,77 @@
 const GET_LANDING_PAGE = /* GraphQL */ `
-  query GET_LANDING_PAGE {
-    landingPage {
-      logo {
+  fragment logo on LandingPage {
+    logo {
+      alternativeText
+      url
+    }
+  }
+
+  fragment header on LandingPage {
+    header {
+      title
+      description
+      button {
+        label
+        url
+      }
+      image {
         alternativeText
         url
       }
+    }
+  }
+
+  fragment sectionAboutProject on LandingPage {
+    sectionAboutProject {
+      image {
+        alternativeText
+        url
+      }
+      title
+      description
+    }
+  }
+
+  fragment sectionTech on LandingPage {
+    sectionTech {
+      title
+      techIcons {
+        title
+        icon {
+          url
+        }
+      }
+    }
+  }
+
+  fragment sectionConcepts on LandingPage {
+    sectionConcepts {
+      title
+      concepts {
+        title
+      }
+    }
+  }
+
+  fragment sectionModules on LandingPage {
+    sectionModules {
+      title
+      modules {
+        title
+        subtitle
+        description
+      }
+    }
+  }
+
+  query GET_LANDING_PAGE {
+    landingPage {
+      ...logo
+      ...header
+      ...sectionAboutProject
+      ...sectionTech
+      ...sectionConcepts
+      ...sectionModules
     }
   }
 `
