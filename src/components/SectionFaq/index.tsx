de915321e57b83dacd67bca848ceb02a6473f50d
gabxyz <gabxyzdev@gmail.com>
2021-11-01 17:39:18 -0300

    modified the component to get the extra question from the api based on a condition

--- src/components/SectionFaq/index.tsx
+++ src/components/SectionFaq/index.tsx
@@ -6,12 +6,17 @@ import Heading from 'components/Heading'
 import * as S from './styles'
 import { SectionFaqProps } from 'types/api'
 
-const SectionFaq = ({ title, questions }: SectionFaqProps) => (
+const SectionFaq = ({
+  title,
+  questions,
+  extraQuestionBool,
+  extraQuestion,
+  extraAnswer
+}: SectionFaqProps) => (
   <S.Wrapper>
     <S.Content>
       <Container>
         <Heading>{title}</Heading>
-
         <S.Questions>
           {questions.map(({ question, answer }, index) => (
             <S.Question key={index}>
@@ -20,24 +25,12 @@ const SectionFaq = ({ title, questions }: SectionFaqProps) => (
             </S.Question>
           ))}
         </S.Questions>
-
-        <S.ExtraQuestion>
-          <Heading lineBottom>Eu tenho outra dúvida!</Heading>
-          <p>
-            Sem problemas! Você pode acessar qualquer uma das{' '}
-            <a
-              href="https://willianjusten.com.br/about"
-              target="_blank"
-              rel="noreferrer"
-            >
-              minhas redes sociais
-            </a>{' '}
-            ou entrar no{' '}
-            <a href="http://bit.ly/slack-will" target="_blank" rel="noreferrer">
-              slack do nosso curso.
-            </a>
-          </p>
-        </S.ExtraQuestion>
+        {extraQuestionBool && (
+          <S.ExtraQuestion>
+            <Heading lineBottom>{extraQuestion}</Heading>
+            <div dangerouslySetInnerHTML={{ __html: extraAnswer }} />
+          </S.ExtraQuestion>
+        )}
       </Container>
     </S.Content>
   </S.Wrapper>
